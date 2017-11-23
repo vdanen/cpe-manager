2fd17a6a9db15c9cc0c4a92f75073d51bbde220e
Vincent Danen <vdanen@redhat.com>
2017-11-23 15:20:10 -0700

    add license info to file

--- cpe-manager.py
+++ cpe-manager.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+"""
+Copyright 2016, 2017
+ Vincent Danen <vdanen@redhat.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License <gnu.org/licenses/gpl.html> for more details.
+"""
+
 import argparse
 import requests
 import sys
